cfa969cd907bad73f0e24d8536cccc3c0c83f472
David de Klerk <dawiedotcom@gmail.com>
2013-06-26 23:54:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# denote
+
+
+## Installation
+
+
+## Usage
+
+
+## Options
+
+
+## License
+
+Copyright © 2013 David de Klerk
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,27 @@
+(defproject denote "0.1.0-SNAPSHOT"
+  :description "A browser based markup editor."
+  :url "http://github.com/dawiedotcom/denote"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]
+                 [ring "1.0.2"]
+                 [fogus/ring-edn "0.1.0"]
+                 [org.clojure/clojurescript "0.0-1806"]
+                 [com.google.javascript/closure-compiler "r1592"]
+                 [org.clojure/google-closure-library "0.0-790"]]
+  :plugins [[lein-ring "0.8.5"]
+            [lein-cljsbuild "0.3.2"]]
+  :ring {:handler denote.server.service/app}
+  :source-paths ["src/clj"]
+  :main denote.server.core
+  :cljsbuild 
+  {:builds[{:source-paths ["src/cljs"]
+            :jar true
+            :compiler 
+            {:output-dir "resources/public/javascript/"
+             :output-to "resources/public/javascript/denote.js"
+             :pretty-print true
+             :optimizations :simple}}]}
+  :profiles
+  {:dev {:dependancies [[ring-mock "0.1.5"]]}})
